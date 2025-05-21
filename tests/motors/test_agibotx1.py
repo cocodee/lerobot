de5c983d097de09ac001333ebafe69ebe8e64508
dikeke <dikeke@qiniu.com>
2025-05-21 12:16:01 +0800

    test motor

--- tests/motors/test_agibotx1.py
+++ tests/motors/test_agibotx1.py
@@ -9,7 +9,7 @@ from lerobot.common.robot_devices.motors.agibotx1 import (
     JointOutOfRangeError,
 )
 import lerobot.common.robot_devices.motors.agibotx1 as agibotx1
-
+import time
 
 class TestAgibotX1MotorsBus(unittest.TestCase):
     def setUp(self):
@@ -26,12 +26,21 @@ class TestAgibotX1MotorsBus(unittest.TestCase):
     @patch("lerobot.common.robot_devices.motors.agibotx1.XyberController.get_instance")
     def test_connect_and_read(self, mock_get_instance):
         agibotx1.create_dcu(self.mock_config.dcu_name,self.mock_config.ethercat_id)
+        self.motor_bus = AgibotX1MotorsBus(self.mock_config)
+
         self.motor_bus.connect()
         ret = agibotx1.start_controller("enp2s0")
-        agibotx1.get_controller().enable_all_actuators()
+        if not ret:
+            print("Start Failed")
+        ret = agibotx1.get_controller().enable_all_actuators()
+        if ret:
+            print("Enable Actuator Success")
+        else:
+            print("Enable Actuator Failed")
         read_data = self.motor_bus.read("position")
         self.assertTrue(self.motor_bus.is_connected)
         self.assertIsInstance(read_data, np.ndarray)
+        time.sleep(10)
         print("read data length:",read_data.shape)
         self.motor_bus.disconnect()
 
