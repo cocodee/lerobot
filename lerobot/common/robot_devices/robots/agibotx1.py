e0c32122671d605a958e72b02798f995d545b7ee
dikeke <dikeke@qiniu.com>
2025-06-05 16:47:05 +0800

    lumbar

--- lerobot/common/robot_devices/robots/agibotx1.py
+++ lerobot/common/robot_devices/robots/agibotx1.py
@@ -206,6 +206,7 @@ class AgibotX1Robot():
             self.follower_arms[name].write("position", goal_pos)
             self.logs[f"write_follower_{name}_goal_pos_dt_s"] = time.perf_counter() - before_fwrite_t
 
+        self.lumbar["main"].write("position", [0,0])
         # Early exit when recording data is not requested
         if not record_data:
             return
