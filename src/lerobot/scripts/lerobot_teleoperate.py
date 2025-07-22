7b1719fd8755e2b7789bad14c82b5d36888fc10a
dikeke <dikeke@qiniu.com>
2025-07-22 16:29:37 +0800

    limit fpx

--- src/lerobot/scripts/lerobot_teleoperate.py
+++ src/lerobot/scripts/lerobot_teleoperate.py
@@ -102,7 +102,7 @@ class TeleoperateConfig:
     teleop: TeleoperatorConfig
     robot: RobotConfig
     # Limit the maximum frames per second.
-    fps: int = 60
+    fps: int = 20
     teleop_time_s: float | None = None
     # Display all cameras on screen
     display_data: bool = False
