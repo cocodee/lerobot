c30d689c55853d5eaf21205bb46039e488729fac
dikeke <dikeke@qiniu.com>
2025-07-22 14:24:30 +0800

    ros2 follower

--- src/lerobot/robots/ros2_follower/config_ros2_follower.py
+++ src/lerobot/robots/ros2_follower/config_ros2_follower.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 from lerobot.cameras import CameraConfig
 
