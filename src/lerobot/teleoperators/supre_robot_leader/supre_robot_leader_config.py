563c9a75d8d8f21cd499918485e9c512d162dccb
dikeke <dikeke@qiniu.com>
2025-09-12 14:30:44 +0800

    leader follower

--- src/lerobot/teleoperators/supre_robot_leader/supre_robot_leader_config.py
+++ src/lerobot/teleoperators/supre_robot_leader/supre_robot_leader_config.py
@@ -2,12 +2,12 @@ from dataclasses import dataclass, field
 from pathlib import Path
 from lerobot.cameras import CameraConfig
 
-from lerobot.robots.config import RobotConfig
+from lerobot.teleoperators.config import TeleoperatorConfig
 
 _DEFAULT_CONFIG_PATH = Path(__file__).resolve().parent / "supre_robot_config.yaml"
     
-@RobotConfig.register_subclass("supre_robot_leader")
+@TeleoperatorConfig.register_subclass("supre_robot_leader")
 @dataclass
-class SupreRobotLeaderConfig(RobotConfig):
+class SupreRobotLeaderConfig(TeleoperatorConfig):
     """Configuration for the SupreRobot."""
     config_path: str = _DEFAULT_CONFIG_PATH
