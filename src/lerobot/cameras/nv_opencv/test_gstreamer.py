f63676cebb3db81f9ea9badc0555b9b0395b0c90
dikeke <dikeke@qiniu.com>
2025-09-12 08:00:15 +0800

    test

--- src/lerobot/cameras/nv_opencv/test_gstreamer.py
+++ src/lerobot/cameras/nv_opencv/test_gstreamer.py
@@ -95,14 +95,14 @@ def create_kitchen_sink_pipeline(
         "appsink drop=true"
     )
 # 1. 创建硬件加速的 GStreamer 管道
-orin_pipeline = create_kitchen_sink_pipeline(
-    device_path="/dev/video0", # 确认这是你的摄像头设备
-    capture_width=640,
-    capture_height=480,
-    framerate=30,
-    output_format="RGB",
-)
-#orin_pipeline = create_software_jpeg_pipeline()
+#orin_pipeline = create_kitchen_sink_pipeline(
+#    device_path="/dev/video0", # 确认这是你的摄像头设备
+#    capture_width=640,
+#    capture_height=480,
+#    framerate=30,
+#    output_format="RGB",
+#)
+orin_pipeline = create_software_jpeg_pipeline()
 print("Using GStreamer pipeline:\n", orin_pipeline)
 
 # 2. 创建配置对象，这次传入 gstreamer_pipeline
