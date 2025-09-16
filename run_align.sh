925eafb22bd0dfcfab4cdf8451b5312f850e0696
dikeke <dikeke@qiniu.com>
2025-09-16 10:19:28 +0800

    align desktop

--- run_align.sh
+++ run_align.sh
@@ -1,5 +1,7 @@
 cd ~/workspace/gitprj/lerobot-env/lerobot
 source ~/miniconda3/etc/profile.d/conda.sh
+echo "Sourcing ROS 2 Humble environment..."
+source /opt/ros/humble/setup.bash
 source ./setup_binding_env.sh
 python ./src/lerobot/teleop_trajectory_align.py \
     --teleop.type=supre_robot_leader \
