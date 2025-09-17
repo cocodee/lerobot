90e6b52f4066b78ad0f77e8657644ab32b5cef62
dikeke <dikeke@qiniu.com>
2025-09-17 10:09:44 +0800

    debug

--- run_binding_teleop.sh
+++ run_binding_teleop.sh
@@ -8,4 +8,5 @@ python -m lerobot.teleoperate \
 --robot.type=supre_robot_follower \
 --robot.id=supre_follower \
 --teleop.type=supre_robot_leader \
---teleop.id=supre_leader
+--teleop.id=supre_leader \
+--config-path=/home/t/workspace/gitprj/lerobot-env/lerobot/src/lerobot/teleoperators/supre_robot_leader/trunk_teleoperate.yaml
