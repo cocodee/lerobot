0d22f0982db685c121a2ee47089136e053b88641
dikeke <dikeke@qiniu.com>
2025-09-17 11:11:32 +0800

    script

--- run_binding_teleop.sh
+++ run_binding_teleop.sh
@@ -4,9 +4,14 @@ python ./src/lerobot/teleop_trajectory_align.py \
     --teleop.type=supre_robot_leader \
     --robot.type=supre_robot_follower
 
+# 构建配置文件的绝对路径
+CONFIG_FILE_PATH="$SCRIPT_DIR/src/lerobot/teleoperators/supre_robot_leader/trunk_teleoperate.yaml"
+
+# 运行 Python 程序
+echo "Using config file: $CONFIG_FILE_PATH"
 python -m lerobot.teleoperate \
---robot.type=supre_robot_follower \
---robot.id=supre_follower \
---teleop.type=supre_robot_leader \
---teleop.id=supre_leader \
---config-path=/home/t/workspace/gitprj/lerobot-env/lerobot/src/lerobot/teleoperators/supre_robot_leader/trunk_teleoperate.yaml
+    --robot.type=supre_robot_follower \
+    --robot.id=supre_follower \
+    --teleop.type=supre_robot_leader \
+    --teleop.id=supre_leader \
+    --config_path="$CONFIG_FILE_PATH"
