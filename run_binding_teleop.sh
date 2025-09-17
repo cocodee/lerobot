2b88ad22c15e25a12843cfcaebb458b6f2aa2074
dikeke <dikeke@qiniu.com>
2025-09-17 11:16:09 +0800

    record

--- run_binding_teleop.sh
+++ run_binding_teleop.sh
@@ -1,12 +1,14 @@
 source ~/miniconda3/etc/profile.d/conda.sh
 source ./setup_binding_env.sh
-python ./src/lerobot/teleop_trajectory_align.py \
-    --teleop.type=supre_robot_leader \
-    --robot.type=supre_robot_follower
+
 
 # 构建配置文件的绝对路径
 CONFIG_FILE_PATH="$SCRIPT_DIR/src/lerobot/teleoperators/supre_robot_leader/trunk_teleoperate.yaml"
 
+python ./src/lerobot/teleop_trajectory_align.py \
+    --teleop.type=supre_robot_leader \
+    --robot.type=supre_robot_follower
+    --config_path="$CONFIG_FILE_PATH"
 # 运行 Python 程序
 echo "Using config file: $CONFIG_FILE_PATH"
 python -m lerobot.teleoperate \
