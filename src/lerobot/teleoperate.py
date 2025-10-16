690a4f6458868d573ed40985bf3b4a48def9b076
dikeke <dikeke@qiniu.com>
2025-10-16 12:26:58 +0800

    debug

--- src/lerobot/teleoperate.py
+++ src/lerobot/teleoperate.py
@@ -117,8 +117,8 @@ def teleop_loop(
 
         robot.send_action(action)
         dt_s = time.perf_counter() - loop_start
+        print(f"loop time: {dt_s * 1e3:.6f}ms ({1 / dt_s:.0f} Hz)")
         busy_wait(1 / fps - dt_s)
-        print(f"Time: {time.perf_counter() - loop_start:.2f}s")
         loop_s = time.perf_counter() - loop_start
 
         print("\n" + "-" * (display_len + 10))
