047fd7958d47a3481c02f18265f3b05d29a1a975
dikeke <dikeke@qiniu.com>
2025-10-30 16:09:57 +0800

    debug

--- zrc_inference_server/inference_config.py
+++ zrc_inference_server/inference_config.py
@@ -27,7 +27,7 @@ class LocalInferenceConfig:
     @classmethod
     def __get_path_fields__(cls) -> list[str]:
         # Allows --local.policy=/path/to/policy from CLI
-        return ["policy"]        
+        return ["local.policy"]        
 
 @dataclass
 class RemoteInferenceConfig:
