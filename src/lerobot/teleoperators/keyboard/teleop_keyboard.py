198a169d7869771ee18b622a3b1c2d63a5db9295
dikeke <dikeke@qiniu.com>
2025-12-09 15:35:01 +0800

    test

--- src/lerobot/teleoperators/keyboard/teleop_keyboard.py
+++ src/lerobot/teleoperators/keyboard/teleop_keyboard.py
@@ -102,7 +102,6 @@ class KeyboardTeleop(Teleoperator):
 
     def _on_press(self, key):
         if hasattr(key, "char"):
-            print(f"Key pressed: {key.char}")
             self.event_queue.put((key.char, True))
 
     def _on_release(self, key):
@@ -180,6 +179,7 @@ class KeyboardEndEffectorTeleop(KeyboardTeleop):
     def _on_press(self, key):
         if hasattr(key, "char"):
             key = key.char
+            print(f"Key pressed: {key.char}")            
         self.event_queue.put((key, True))
 
     def _on_release(self, key):
