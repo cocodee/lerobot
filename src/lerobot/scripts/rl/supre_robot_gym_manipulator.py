988799819fbdf211500f6d4fbd2b9a2e4e07c105
dikeke <dikeke@qiniu.com>
2025-12-09 14:46:43 +0800

    debug

--- src/lerobot/scripts/rl/supre_robot_gym_manipulator.py
+++ src/lerobot/scripts/rl/supre_robot_gym_manipulator.py
@@ -1827,7 +1827,7 @@ class KeyboardControlWrapper(GamepadControlWrapper):
             action_list.append(float(gripper_val))
 
         gamepad_action_np = np.array(action_list, dtype=np.float32)
-
+        print(f"is_intervention_active: {self.is_intervention_active},action_list: {action_list}")
         return (
             self.is_intervention_active,
             gamepad_action_np,
