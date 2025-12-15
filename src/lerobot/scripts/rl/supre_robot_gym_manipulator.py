02fef52661e543a9244be312b8c2a05b5cc53aa1
dikeke <dikeke@qiniu.com>
2025-12-15 10:43:59 +0800

    sim_robot

--- src/lerobot/scripts/rl/supre_robot_gym_manipulator.py
+++ src/lerobot/scripts/rl/supre_robot_gym_manipulator.py
@@ -55,13 +55,12 @@ from lerobot.model.kinematics import RobotKinematics
 from lerobot.robots import (  # noqa: F401
     RobotConfig,
     make_robot_from_config,
-    supre_robot_follower
+    sim_robot,
 )
 from lerobot.teleoperators import (
     gamepad,  # noqa: F401
     keyboard,  # noqa: F401
     make_teleoperator_from_config,
-    supre_robot_leader
 )
 from lerobot.teleoperators.gamepad.teleop_gamepad import GamepadTeleop
 from lerobot.teleoperators.keyboard.teleop_keyboard import KeyboardEndEffectorTeleop
