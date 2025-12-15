67d5740a9f9669f87f508a9cf49173ea5409701d
dikeke <dikeke@qiniu.com>
2025-12-15 15:18:34 +0800

    test

--- src/lerobot/teleoperators/keyboard/teleop_keyboard.py
+++ src/lerobot/teleoperators/keyboard/teleop_keyboard.py
@@ -187,6 +187,7 @@ class KeyboardEndEffectorTeleop(KeyboardTeleop):
     def _on_release(self, key):
         if hasattr(key, "char"):
             key = key.char
+            print(f"Key released: {key}")
         self.event_queue.put((key, False))
 
     def get_action(self) -> dict[str, Any]:
