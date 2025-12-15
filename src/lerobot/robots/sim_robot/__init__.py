c409129f17069dc306c7da74d9b06bb71bd19e75
dikeke <dikeke@qiniu.com>
2025-12-15 11:29:50 +0800

    test

--- src/lerobot/robots/sim_robot/__init__.py
+++ src/lerobot/robots/sim_robot/__init__.py
@@ -1,2 +1,3 @@
 from .config_sim_robot import SimRobotConfig, SimRobotHilConfig
-from .sim_robot import SimRobot, SimRobotHil
+from .sim_robot import SimRobot
+from .sim_robot_hil import SimRobotHil
