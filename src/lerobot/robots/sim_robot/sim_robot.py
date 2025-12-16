ae88fcf4bfb2d2fce06cc5d9a81101ae39b8e500
dikeke <dikeke@qiniu.com>
2025-12-16 17:52:56 +0800

    test' i

--- src/lerobot/robots/sim_robot/sim_robot.py
+++ src/lerobot/robots/sim_robot/sim_robot.py
@@ -137,13 +137,14 @@ class SimRobot(Robot):
 
         # import pdb; pdb.set_trace()
         # 机器人关节名称和仿真名称映射
+        print(f"action: {action}")
         if "left_arm_joint_1.pos" in action:
             new_action = {}
             for key, val in action.items():
                 name = self.robot2sim[key.split(".")[0]]
                 new_action[f"{name}.pos"] = val
             action = new_action
-        
+        print(f"new_action: {new_action}")
         # import pdb; pdb.set_trace()
         goal_pos = {key.removesuffix(".pos"): val for key, val in action.items() if key.endswith(".pos")}
         # Cap goal position when too far away from present position.
