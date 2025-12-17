3084fa9786cf9ca11c174da55bc64a1be03869fb
dikeke <dikeke@qiniu.com>
2025-12-17 16:05:11 +0800

    test

--- src/lerobot/robots/sim_robot/sim_robot_hil.py
+++ src/lerobot/robots/sim_robot/sim_robot_hil.py
@@ -204,7 +204,7 @@ class SimRobotHil(SimRobot):
         joint_positions, _ = self.simulator.get_joint_states()
         
         return {
-            self.sim2robot[name]: math.degrees(joint_positions[i])
+            self.sim2robot[name]: math.degrees(joint_positions[i])*self.joint_direction[i]
             for i, name in enumerate(self.joint_names)
         }
 
