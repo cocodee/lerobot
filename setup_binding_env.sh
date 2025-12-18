13a59add23f58b3d7893194e04551472f7fbc44e
dikeke <dikeke@qiniu.com>
2025-12-18 22:53:15 +0800

    remove

--- setup_binding_env.sh
+++ setup_binding_env.sh
@@ -2,6 +2,6 @@
 conda activate ros2_env
 sudo chmod 666 /dev/ttyTHS1
 sudo chmod 666 /dev/ttyTHS2
-export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
+#export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 export LD_PRELOAD=/usr/lib/aarch64-linux-gnu/libstdc++.so.6 python
 export export PYTHONPATH=/home/t/workspace/misumi_grippper/src/build:/home/t/workspace/eu_motor/build/lib:$PYTHONPATH
