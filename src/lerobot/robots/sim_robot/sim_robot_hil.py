606f29a34bca13d8e7370a15fbba47440551f5c4
dikeke <dikeke@qiniu.com>
2025-12-24 11:40:31 +0800

    test

--- src/lerobot/robots/sim_robot/sim_robot_hil.py
+++ src/lerobot/robots/sim_robot/sim_robot_hil.py
@@ -161,7 +161,7 @@ class SimRobotHil(SimRobot):
         logger.info(f"current_joint_pos: {self.current_joint_pos}")
         logger.info(f"target_joint_values_in_degrees: {target_joint_values_in_degrees}")
 
-        #self._debug_draw_frame(desired_ee_pos, label="Target", life_time=0.1)
+        self._debug_draw_frame(desired_ee_pos, label="Target", life_time=0.5)
         
         # 2. 画出当前的实际位置 (Actual)
         self._debug_draw_frame(self.current_ee_pos, label="Current", life_time=0.5)        
