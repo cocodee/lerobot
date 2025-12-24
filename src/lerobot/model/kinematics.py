860faaa6f010fc5783a133ad8f324161c215097a
dikeke <dikeke@qiniu.com>
2025-12-24 17:28:04 +0800

    solve enable

--- src/lerobot/model/kinematics.py
+++ src/lerobot/model/kinematics.py
@@ -54,6 +54,14 @@ class RobotKinematics:
         # Initialize frame task for IK
         self.tip_frame = self.solver.add_frame_task(self.target_frame_name, np.eye(4))
 
+        for joint_name in self.robot.joint_names():
+            if joint_name in self.joint_names:
+                # 如果是左臂关节，允许求解器移动它
+                self.solver.enable_joint(joint_name, True)
+            else:
+                # 如果是其他关节（右臂、腿、头等），禁止求解器移动它
+                self.solver.enable_joint(joint_name, False)
+
     def forward_kinematics(self, joint_pos_deg):
         """
         Compute forward kinematics for given joint configuration given the target frame name in the constructor.
