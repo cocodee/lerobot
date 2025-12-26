369b34259fd62bb6aca794a7519543dd30cf1615
dikeke <dikeke@qiniu.com>
2025-12-26 15:11:06 +0800

    axis"

--- src/lerobot/teleoperators/webxr/teleop_webxr.py
+++ src/lerobot/teleoperators/webxr/teleop_webxr.py
@@ -150,8 +150,8 @@ class WebxrTeleop(Teleoperator):
         #   WebXR -Z (前) -> Robot X (前)
         #   WebXR -X (左) -> Robot Y (左)  (注意：WebXR X是右，所以取反)
         #   WebXR  Y (上) -> Robot Z (上)
-        # 红色=X轴(-Right), 绿色=Y轴(-Forward), 蓝色=Z轴(Up)
-        # (WebXR 坐标系: Y-Up, Z -Forward, X Right)
+        # 红色=X轴(-Right), 绿色=Y轴(UP), 蓝色=Z轴(Forward)基座标系
+        # (WebXR 坐标系: Y Up, Z -Forward, X Right)
         scale = self.config.pos_scale
         
         #curr_pos = np.array([
@@ -162,8 +162,8 @@ class WebxrTeleop(Teleoperator):
 
         curr_pos = np.array([
              -raw_p[0] * scale, # Robot X
-             raw_p[2] * scale, # Robot Y
-             raw_p[1] * scale  # Robot Z
+             raw_p[1] * scale, # Robot Y
+             -raw_p[2] * scale  # Robot Z
         ])
         # 处理旋转
         xr_rot = R.from_quat(raw_q) # WebXR 的原始旋转
