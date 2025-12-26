f19cc5ab240d6685fa8f1045bfbfd94e11ac0fad
dikeke <dikeke@qiniu.com>
2025-12-26 16:41:42 +0800

    action

--- src/lerobot/scripts/rl/supre_robot_gym_manipulator.py
+++ src/lerobot/scripts/rl/supre_robot_gym_manipulator.py
@@ -408,11 +408,11 @@ class RobotEnv(gym.Env):
                 - truncated (bool): True if the episode was truncated (e.g., time constraints).
                 - info (dict): Additional debugging information including intervention status.
         """
-        action_dict = {"delta_x": action[0], "delta_y": action[1], "delta_z": action[2]}
+        #action_dict = {"delta_x": action[0], "delta_y": action[1], "delta_z": action[2]}
 
         # 1.0 action corresponds to no-op action
-        action_dict["gripper"] = action[3] if self.use_gripper else 1.0
-
+        #action_dict["gripper"] = action[3] if self.use_gripper else 1.0
+        action_dict = action
         self.robot.send_action(action_dict)
 
         self._get_observation()
