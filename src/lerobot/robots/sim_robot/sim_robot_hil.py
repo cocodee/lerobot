9556fc8b59388486e052116aeb8b50d5204940cf
dikeke <dikeke@qiniu.com>
2025-12-29 12:05:19 +0800

    abs rotation

--- src/lerobot/robots/sim_robot/sim_robot_hil.py
+++ src/lerobot/robots/sim_robot/sim_robot_hil.py
@@ -68,7 +68,8 @@ class SimRobotHil(SimRobot):
         return {
             "dtype": "float32",
             "shape": (4,),
-            "names": {"delta_x": 0, "delta_y": 1, "delta_z": 2, "gripper": 3},
+            "names": {"delta_x": 0, "delta_y": 1, "delta_z": 2, "delta_qx":3, "delta_qy":4,
+                      "delta_qz":5, "delta_qw":6, "gripper": 7},
         }
 
     def send_action(self, action: dict[str, Any]) -> dict[str, Any]:
@@ -132,21 +133,28 @@ class SimRobotHil(SimRobot):
         if True:
             self.current_ee_pos = self.kinematics.forward_kinematics(self.current_joint_pos[:-1])
 
-        current_rot_mat = self.current_ee_pos[:3, :3]
-        delta_rot_mat = np.reshape(p.getMatrixFromQuaternion(delta_quat), (3, 3))
-        
-        # 应用旋转增量
-        new_rot_mat = delta_rot_mat @ current_rot_mat
-        
+        ###current_rot_mat = self.current_ee_pos[:3, :3]
+        ###delta_rot_mat = np.reshape(p.getMatrixFromQuaternion(delta_quat), (3, 3))
+        ###
+        #### 应用旋转增量
+        ###new_rot_mat = delta_rot_mat @ current_rot_mat
+        ###
+###
+        ###logger.info(f"Current end-effector position: {self.current_ee_pos}")
+        #### Set desired end-effector position by adding delta
+        ###desired_ee_pos = np.eye(4)
+        ####desired_ee_pos[:3, :3] = self.current_ee_pos[:3, :3]  # Keep orientation
+        ###desired_ee_pos[:3, :3] = new_rot_mat
 
-        logger.info(f"Current end-effector position: {self.current_ee_pos}")
-        # Set desired end-effector position by adding delta
+
+        # --- 旋转部分：直接使用传入的绝对姿态 ---
+        # 因为 WebXR 那边已经处理好了 R_base * R_xr，所以这里直接转换即可
         desired_ee_pos = np.eye(4)
-        #desired_ee_pos[:3, :3] = self.current_ee_pos[:3, :3]  # Keep orientation
+        new_rot_mat = np.reshape(p.getMatrixFromQuaternion(delta_quat), (3, 3))
         desired_ee_pos[:3, :3] = new_rot_mat
-
         # Add delta to position and clip to bounds
         desired_ee_pos[:3, 3] = self.current_ee_pos[:3, 3] + action[:3]
+
         if self.end_effector_bounds is not None:
             logger.info(f"Clip desired end-effector position to bounds {self.end_effector_bounds}")
             desired_ee_pos[:3, 3] = np.clip(
