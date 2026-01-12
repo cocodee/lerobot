beb0a94ce187b1cd486150d9e3417c19966b5da6
dikeke <dikeke@qiniu.com>
2026-01-12 09:59:04 +0800

    action dim

--- src/lerobot/scripts/rl/supre_robot_gym_manipulator.py
+++ src/lerobot/scripts/rl/supre_robot_gym_manipulator.py
@@ -2345,7 +2345,10 @@ def record_dataset(env, policy, cfg):
 
     # Setup initial action (zero action if using teleop)
     #action = env.action_space.sample() * 0.0
-    action = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0], dtype=np.float32)
+    if cfg.wrapper.use_gripper:
+        action = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0], dtype=np.float32)
+    else:
+        action = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0,0.0], dtype=np.float32)
     action = torch.as_tensor(action, dtype=torch.float32,device=torch.device("cpu"))
     action_names = ["delta_x_ee", "delta_y_ee", "delta_z_ee","delta_qx","delta_qy","delta_qz","delta_qw"]
     if cfg.wrapper.use_gripper:
