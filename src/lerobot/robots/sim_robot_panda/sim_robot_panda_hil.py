69c188b633866836a16076e52bcba330774fc7b7
dikeke <dikeke@qiniu.com>
2026-01-14 15:38:14 +0800

    bekinematics

--- src/lerobot/robots/sim_robot_panda/sim_robot_panda_hil.py
+++ src/lerobot/robots/sim_robot_panda/sim_robot_panda_hil.py
@@ -10,7 +10,7 @@ from typing import Any, Dict, List, Optional
 from scipy.spatial.transform import Rotation as R
 
 from lerobot.errors import DeviceAlreadyConnectedError, DeviceNotConnectedError
-from lerobot.model.kinematics import RobotKinematics
+from lerobot.model.kinematics import RobotKinematics, BeRobotKinematics
 from ..sim_robot.config_sim_robot import SimRobotPandaHilConfig
 from .sim_robot_panda import SimRobotPanda  # 确保这里导入的是 MuJoCo 版本的 SimRobot
 
@@ -41,7 +41,7 @@ class SimRobotPandaHil(SimRobotPanda):
                 "Ensure config.urdf_path points to 'panda.urdf'."
             )
 
-        self.kinematics = RobotKinematics(
+        self.kinematics = BeRobotKinematics(
             urdf_path=self.config.urdf_path,
             target_frame_name=self.config.target_frame_name, # 通常是 "panda_link8" 或 "panda_hand"
             joint_names=PANDA_URDF_JOINT_NAMES,
