921c625713cfa4321c31b83f0bf318cd5b1b0e01
dikeke <dikeke@qiniu.com>
2026-04-16 16:14:27 +0800

    test

--- setup_binding_env.sh
+++ setup_binding_env.sh
@@ -4,4 +4,4 @@ sudo chmod 666 /dev/ttyTHS1
 sudo chmod 666 /dev/ttyTHS2
 #export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 export LD_PRELOAD=/usr/lib/aarch64-linux-gnu/libstdc++.so.6 python
-export export PYTHONPATH=/home/t/workspace/misumi_grippper/src/build:/home/t/workspace/eu_motor/build/lib:$PYTHONPATH
+export PYTHONPATH=/home/t/workspace/misumi_grippper/src/build:/home/t/workspace/eu_motor/build/lib:$PYTHONPATH
